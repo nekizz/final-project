9b8be2506d5edb8097d1c6a5ff2ccbdc0bb18b71
minhnq2 <minhnguyenquang@bizfly.vn>
2022-11-17 09:02:56 +0700

    create message service

---
+++ backend/message/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+func main() {
+
+	from := "drminhvipoi2@gmail.com"
+	password := "minhminh123"
+
+	toEmailAddress := "drminhvipoi2000@gmail.com"
+	to := []string{toEmailAddress}
+
+	host := "smtp.gmail.com"
+	port := "587"
+	address := host + ":" + port
+
+	subject := "Subject: This is the subject of the mail\n"
+	body := "This is the body of the mail"
+	message := []byte(subject + body)
+
+	auth := smtp.PlainAuth("", from, password, host)
+
+	err := smtp.SendMail(address, auth, from, to, message)
+	if err != nil {
+		fmt.Println(err)
+	}
+}

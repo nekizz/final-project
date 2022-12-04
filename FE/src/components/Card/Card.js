abb4e14f15b44ceda01c062bd1020a16b113da09
HuyenLinhh <linhhuyen.2k@gmail.com>
2022-12-04 17:34:16 +0700

    fake api

--- FE/src/components/Card/Card.js
+++ FE/src/components/Card/Card.js
@@ -1,129 +1,122 @@
-import React from "react";
-import classNames from "classnames/bind";
-import styles from "./Card.module.css";
-import avatar from "../../assets/img/avatar.jpg";
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Card.module.css';
+import avatar from '../../assets/img/avatar.jpg';
 
-import {
-  AiOutlineHeart,
-  AiOutlineStar,
-  AiOutlineCar,
-  AiOutlineClose,
-} from "react-icons/ai";
-import { IoAdd } from "react-icons/io5";
-import { BiBed, BiBath } from "react-icons/bi";
-import { MdPets } from "react-icons/md";
+import { AiOutlineHeart, AiOutlineStar, AiOutlineCar, AiOutlineClose } from 'react-icons/ai';
+import { IoAdd } from 'react-icons/io5';
+import { BiBed, BiBath } from 'react-icons/bi';
+import { MdPets } from 'react-icons/md';
 const cx = classNames.bind(styles);
 
 function Card({
-  star = false,
-  featured = false,
-  row = false,
-  guide = false,
-  wishlists = false,
-  add = false,
-  onClick,
-  desc,
-  children,
-  className,
-  mb300,
-}) {
-  let classes =
-    featured && !guide && !row
-      ? cx("feature-container")
-      : featured && guide && !row
-      ? cx("guide-container")
-      : row && featured && !guide
-      ? cx("feature-row")
-      : wishlists && !row && !featured && !guide
-      ? cx("wishlists-container")
-      : cx("card-container");
-  const cardClasses = cx("card", {
-    featured,
-    wishlists,
-    row,
-    add,
+    star = false,
+    featured = false,
+    row = false,
+    guide = false,
+    wishlists = false,
+    add = false,
+    onClick,
+    desc,
+    children,
+    className,
     mb300,
-    [className]: className,
-  });
+}) {
+    let classes =
+        featured && !guide && !row
+            ? cx('feature-container')
+            : featured && guide && !row
+            ? cx('guide-container')
+            : row && featured && !guide
+            ? cx('feature-row')
+            : wishlists && !row && !featured && !guide
+            ? cx('wishlists-container')
+            : cx('card-container');
+    const cardClasses = cx('card', {
+        featured,
+        wishlists,
+        row,
+        add,
+        mb300,
+        [className]: className,
+    });
 
-  const props = { onClick };
-  return (
-    <div className={cardClasses}>
-      {star && (
-        <div className={cx("star-rated")}>
-          <AiOutlineStar />
-          <AiOutlineStar />
-          <AiOutlineStar />
-          <AiOutlineStar />
-          <AiOutlineStar />
-        </div>
-      )}
-      {!guide && !row && !wishlists && (
-        <div className={cx("icon-heart")}>
-          <AiOutlineHeart />
-        </div>
-      )}
-      {wishlists && !add && (
-        <i className={cx("close-icon")} {...props}>
-          <AiOutlineClose />
-        </i>
-      )}
-      {featured && !row && !guide && (
-        <div className={cx("feature-information")}>
-          <span className={cx("price")}>$ 1000 - 5000 USD</span>
-          <div className={cx("circle-wrapper")}>
-            <span className={cx("circle")}></span>
-            <span className={cx("circle")}></span>
-            <span className={cx("circle")}></span>
-            <span className={cx("circle")}></span>
-          </div>
-        </div>
-      )}
-      {add ? (
-        <i className={cx("add-icon")} {...props}>
-          <IoAdd />
-        </i>
-      ) : (
-        <div className={classes}>
-          {!featured && !wishlists && (
-            <img className={cx("card-avatar")} src={avatar} alt="hieu" />
-          )}
-          {wishlists ? (
-            <div>
-              <h3 className={cx("wishlist-title")}>{children}</h3>
-              <span className={cx("wishlist-desc")}>{desc}</span>
-            </div>
-          ) : (
-            <div>
-              <h3 className={cx("card-title")}>yeu huong</h3>
-              <span className={cx("card-desc")}>yeu huong vl</span>
-            </div>
-          )}
+    const props = { onClick };
+    return (
+        <div className={cardClasses}>
+            {star && (
+                <div className={cx('star-rated')}>
+                    <AiOutlineStar />
+                    <AiOutlineStar />
+                    <AiOutlineStar />
+                    <AiOutlineStar />
+                    <AiOutlineStar />
+                </div>
+            )}
+            {!guide && !row && !wishlists && (
+                <div className={cx('icon-heart')}>
+                    <AiOutlineHeart />
+                </div>
+            )}
+            {wishlists && !add && (
+                <i className={cx('close-icon')} {...props}>
+                    <AiOutlineClose />
+                </i>
+            )}
+            {featured && !row && !guide && (
+                <div className={cx('feature-information')}>
+                    <span className={cx('price')}>$ 1000 - 5000 USD</span>
+                    <div className={cx('circle-wrapper')}>
+                        <span className={cx('circle')}></span>
+                        <span className={cx('circle')}></span>
+                        <span className={cx('circle')}></span>
+                        <span className={cx('circle')}></span>
+                    </div>
+                </div>
+            )}
+            {add ? (
+                <i className={cx('add-icon')} {...props}>
+                    <IoAdd />
+                </i>
+            ) : (
+                <div className={classes}>
+                    {!featured && !wishlists && <img className={cx('card-avatar')} src={avatar} alt="hieu" />}
+                    {wishlists ? (
+                        <div>
+                            <h3 className={cx('wishlist-title')}>{children}</h3>
+                            <span className={cx('wishlist-desc')}>{desc}</span>
+                        </div>
+                    ) : (
+                        <div>
+                            <h3 className={cx('card-title')}>L'Etape Gasconne</h3>
+                            <span className={cx('card-desc')}>11,Allemans-du-Dropt</span>
+                        </div>
+                    )}
 
-          {featured && !guide && (
-            <div className={cx("feature-icons")}>
-              <div className={cx("icon")}>
-                <BiBed />
-                <span>1</span>
-              </div>
-              <div className={cx("icon")}>
-                <BiBath />
-                <span>1</span>
-              </div>
-              <div className={cx("icon")}>
-                <AiOutlineCar />
-                <span>1</span>
-              </div>
-              <div className={cx("icon")}>
-                <MdPets />
-                <span>1</span>
-              </div>
-            </div>
-          )}
+                    {featured && !guide && (
+                        <div className={cx('feature-icons')}>
+                            <div className={cx('icon')}>
+                                <BiBed />
+                                <span>1</span>
+                            </div>
+                            <div className={cx('icon')}>
+                                <BiBath />
+                                <span>1</span>
+                            </div>
+                            <div className={cx('icon')}>
+                                <AiOutlineCar />
+                                <span>1</span>
+                            </div>
+                            <div className={cx('icon')}>
+                                <MdPets />
+                                <span>1</span>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default Card;
--- FE/src/components/top-destinations/top-destionations.js
+++ FE/src/components/top-destinations/top-destionations.js
@@ -1,72 +1,60 @@
-import React from "react";
-import styles from "./top-destinations.module.css";
-import imga from "../../assets/img/download.jpg";
-import classNames from "classnames/bind";
-import { Link } from "react-router-dom";
+import React from 'react';
+import styles from './top-destinations.module.css';
+import imga from '../../assets/img/download.jpg';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 const topDestinations = [
-  {
-    state: "Paraty",
-    city: "RIO DE JANEIRO",
-  },
-  {
-    state: "Morretes",
-    city: "PARANA",
-  },
-  {
-    state: "Olinda",
-    city: "PERNAMBUCO",
-  },
-  {
-    state: "Gramado",
-    city: "RIO GRANDE DO SUL",
-  },
+    {
+        state: 'Paraty',
+        city: 'RIO DE JANEIRO',
+    },
+    {
+        state: 'Morretes',
+        city: 'PARANA',
+    },
+    {
+        state: 'Olinda',
+        city: 'PERNAMBUCO',
+    },
+    {
+        state: 'Gramado',
+        city: 'RIO GRANDE DO SUL',
+    },
 ];
 
 function TopDestinations() {
-  return (
-    <div className={cx("top-destinations")}>
-      <div className={cx("top-destinations__content")}>
-        <h2 className={cx("top-destinations__top")}>top</h2>
-        <h2 className={cx("top-destinations__dest")}>destinations</h2>
-        <p className={cx("top-destinations__desc")}>
-          it's hard enough deciding to move, you don't have to worry about where
-          to move to. These are some of the most popular and best locations to
-          move to based on a number of factors.
-        </p>
-      </div>
-
-      <div className={cx("top-destinations-container")}>
-        {topDestinations.map((destination, index) => (
-          <div className={cx("top-destinations-item")} key={index}>
-            <div className={cx("front")}>
-              <Link
-                to="/HomeBooking#featured"
-                style={{ width: "100%", height: "100%" }}
-              >
-                <img
-                  src={imga}
-                  className={cx("top-destinations-item__img")}
-                  alt=""
-                />
-              </Link>
-              <div className={cx("top-destinations-info")}>
-                <h3 className={cx("top-destinations-state")}>
-                  {destination.state}
-                </h3>
-                <p className={cx("top-destinations-city")}>
-                  {destination.city}
+    return (
+        <div className={cx('top-destinations')}>
+            <div className={cx('top-destinations__content')}>
+                <h2 className={cx('top-destinations__top')}>top</h2>
+                <h2 className={cx('top-destinations__dest')}>destinations</h2>
+                <p className={cx('top-destinations__desc')}>
+                    it's hard enough deciding to move, you don't have to worry about where to move to. These are some of the most popular and best
+                    locations to move to based on a number of factors.
                 </p>
-              </div>
             </div>
-            <div className={cx("back")}>hehe</div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+            <div className={cx('top-destinations-container')}>
+                {topDestinations.map((destination, index) => (
+                    <div className={cx('top-destinations-item')} key={index}>
+                        <div className={cx('front')}>
+                            <Link to="/HomeBooking#featured" style={{ width: '100%', height: '100%' }}>
+                                <img src={imga} className={cx('top-destinations-item__img')} alt="" />
+                            </Link>
+                            <div className={cx('top-destinations-info')}>
+                                <h3 className={cx('top-destinations-state')}>{destination.state}</h3>
+                                <p className={cx('top-destinations-city')}>{destination.city}</p>
+                            </div>
+                        </div>
+                        <div className={cx('back')}>hehe</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default TopDestinations;
